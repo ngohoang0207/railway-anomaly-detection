db9a876f50348d478c8e633357990835e27963fe
ngohoang0207 <166041144+ngohoang0207@users.noreply.github.com>
2024-12-29 00:13:09 +0700

    Update generate_image_crops.py

--- dataset_generation/generate_image_crops.py
+++ dataset_generation/generate_image_crops.py
@@ -65,12 +65,12 @@ def main(args):
         with open(json_path) as json_file:
             json_data = json.load(json_file)
         # Create image crops from visualization_image
-     if MODE == "rs19":
+        if MODE == "rs19":
     visualization_image, cropped_images, cropped_segmentations, _, _, frame \
         = create_image_crops(args, json_data, image, seg)
-     elif MODE == "fishyrails":
-    visualization_image, cropped_images, cropped_segmentations, cropped_orig_images, cropped_orig_segmentations, frame \
-        = create_image_crops(args, json_data, image, seg, image_orig, seg_orig)
+        if MODE == "rs19":
+    visualization_image, cropped_images, cropped_segmentations, _, _, frame \
+        = create_image_crops(args, json_data, image, seg)
         # visualization_image, cropped_images, cropped_segmentations, cropped_orig_images, cropped_orig_segmentations, frame \
         #     = create_image_crops(args, json_data, image, seg, image_orig, seg_orig)
         # Store visualization and visualization_image crops
