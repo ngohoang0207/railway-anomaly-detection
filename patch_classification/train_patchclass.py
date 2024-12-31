6a6ce4e9700e503af739824757f9d5bf7428a13f
ngohoang0207 <166041144+ngohoang0207@users.noreply.github.com>
2024-12-31 22:36:51 +0700

    save epoch

--- patch_classification/train_patchclass.py
+++ patch_classification/train_patchclass.py
@@ -258,9 +258,9 @@ def main(args):
             "epoch": epoch,
             "args": args,
         }
-        if epoch % args.save_freq == 0:
+        # if epoch % args.save_freq == 0:
             utils.save_on_master(checkpoint, os.path.join(args.output_dir, f"model_{epoch}.pth"))
-        utils.save_on_master(checkpoint, os.path.join(args.output_dir, "checkpoint.pth"))
+        # utils.save_on_master(checkpoint, os.path.join(args.output_dir, "checkpoint.pth"))
 
     writer.flush()
 
